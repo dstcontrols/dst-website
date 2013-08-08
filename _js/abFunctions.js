f1e8532adf55cf71983439ac4d56e116e203aa5d
Adrian Birkett <adrian@birkettfamily.com>
2013-08-08 17:28:11 -0600

    Updates required for implementing DST Quality Policy

--- _js/abFunctions.js
+++ _js/abFunctions.js
@@ -1,7 +1,11 @@
 // JavaScript Document
 
-function openWin(img){
+function openWinA(img){
 	var path = "_images/"
-	window.open(path+img,"certWin","menubar=0,resizable=0,width=536,height=678")
+	window.open(path+img,"certWin","menubar=0,resizable=0,location=no,width=536,height=678")
 }
-	
+
+function openWinB(img2){
+	var path = "_images/"
+	window.open(path+img2,"qpWin","menubar=0,resizable=0,location=no,width=480,height=500,left=800")
+}
