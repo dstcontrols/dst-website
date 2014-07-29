6b0779482a44ecddfd8ba2e1965165881e727c5c
Andrew Pong <andrew@awp.io>
2014-07-28 21:41:07 -0700

    Refactored

--- dst-website/Global.asax.cs
+++ dst-website/Global.asax.cs
@@ -4,15 +4,13 @@
 
     using System;
     using System.Web;
-    using BetterCms.Core;
-    using BetterCms.Core.Environment.Host;
     using System.Web.Http;
     using System.Web.Mvc;
+    using BetterCms.Core;
+    using BetterCms.Core.Environment.Host;
 
     #endregion
 
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : HttpApplication
     {
         private static ICmsHost cmsHost;
@@ -21,67 +19,33 @@
         {
             cmsHost = CmsContext.RegisterHost();
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            //    RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-
-            /* DO NOT FORGET TO REMOVE DEFAULT ROUTE REGISTRATION! 
-               FOLLOWING SOURCE CODE SHOULD BE REMOVED: 
-
-               routes.MapRoute(
-                        name: "Default",
-                        url: "{controller}/{action}/{id}",
-                        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                    );
-            */
-
-            // [YOUR CODE]  
-
             cmsHost.OnApplicationStart(this);
         }
 
         protected void Application_BeginRequest()
         {
-            // [YOUR CODE]
-
             cmsHost.OnBeginRequest(this);
         }
 
         protected void Application_EndRequest()
         {
-            // [YOUR CODE]
-
             cmsHost.OnEndRequest(this);
         }
 
         protected void Application_Error()
         {
-            // [YOUR CODE]
-
             cmsHost.OnApplicationError(this);
         }
 
         protected void Application_End()
         {
-            // [YOUR CODE]
-
             cmsHost.OnApplicationEnd(this);
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-            // [YOUR CODE]
-
-            // Uncomment following source code for a quick Better CMS test if you don't have implemented users authentication. 
-            // Do not use this code for production!
-            /*
-            var roles = new[] { "BcmsEditContent", "BcmsPublishContent", "BcmsDeleteContent", "BcmsAdministration" };
-            var principal = new GenericPrincipal(new GenericIdentity("TestUser"), roles);
-            HttpContext.Current.User = principal;
-            */
-
             cmsHost.OnAuthenticateRequest(this);
         }
     }
