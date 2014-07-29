970ba0643818e7fb572d0743cf85bdf39747730b
Andrew Pong <andrew@awp.io>
2014-07-28 23:53:13 -0700

    Using string.empty

--- dst-website/App_Start/RouteConfig.cs
+++ dst-website/App_Start/RouteConfig.cs
@@ -16,25 +16,23 @@
             routes.MapRoute("contacts-form", "contacts/submit-form",
                             new {area = string.Empty, controller = "Contact", action = "ContactForm"},
                             new[] {"dst_website.Controllers"});
-            routes.MapRoute("menu-top", "menu/top", new {area = string.Empty, controller = "SiteMap", action = "Index"},
+            routes.MapRoute("menu-top", "menu/top", 
+                            new {area = string.Empty, controller = "SiteMap", action = "Index"},
                             new[] {"dst_website.Controllers"});
             routes.MapRoute("menu-sub", "menu/submenu/{parentUrl}",
                             new {area = string.Empty, controller = "SiteMap", action = "SubMenu"},
                             new[] {"dst_website.Controllers"});
             routes.MapRoute("blog-latest", "blog/latest/{categoryId}",
-                            new
-                            {
-                                area = string.Empty,
-                                controller = "Blog",
-                                action = "Index",
-                                categoryId = UrlParameter.Optional
-                            }, new[] {"dst_website.Controllers"});
-            routes.MapRoute("blog-last", "blog/last", new {area = string.Empty, controller = "Blog", action = "Last"},
+                            new {area = string.Empty, controller = "Blog", action = "Index", categoryId = UrlParameter.Optional}, 
+                            new[] {"dst_website.Controllers"});
+            routes.MapRoute("blog-last", "blog/last", 
+                            new {area = string.Empty, controller = "Blog", action = "Last"},
                             new[] {"dst_website.Controllers"});
             routes.MapRoute("blog-categories", "blog/categories",
                             new {area = string.Empty, controller = "Blog", action = "GetCategories"},
                             new[] {"dst_website.Controllers"});
-            routes.MapRoute("blog-feed", "blog/feed", new {area = string.Empty, controller = "Blog", action = "Feed"},
+            routes.MapRoute("blog-feed", "blog/feed", 
+                            new {area = string.Empty, controller = "Blog", action = "Feed"},
                             new[] {"dst_website.Controllers"});
         }
     }
