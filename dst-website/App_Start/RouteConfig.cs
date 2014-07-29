648e7655b8541c94715a999856af54bb40d440f0
Andrew Pong <andrew@awp.io>
2014-07-28 20:59:13 -0700

    Configured BetterCMS

--- dst-website/App_Start/RouteConfig.cs
+++ dst-website/App_Start/RouteConfig.cs
@@ -13,11 +13,11 @@ namespace dst_website
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
