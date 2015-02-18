bb69901ed53c9b217d58dd92c6d046d0f9a1c0af
Andrew Pong <a@awp.io>
2015-02-18 11:51:06 -0800

    updated tracking script, changed contact info

--- _js/abFunctions.js
+++ _js/abFunctions.js
@@ -1,11 +1,10 @@
 // JavaScript Document
-
-function openWinA(img){
-	var path = "_images/"
-	window.open(path+img,"certWin","menubar=0,resizable=0,location=no,width=536,height=678")
+function openWinA(img) {
+  var path = "_images/"
+  window.open(path + img, "certWin", "menubar=0,resizable=0,location=no,width=536,height=678")
 }
 
-function openWinB(img2){
-	var path = "_images/"
-	window.open(path+img2,"qpWin","menubar=0,resizable=0,location=no,width=480,height=500,left=800")
+function openWinB(img2) {
+  var path = "_images/"
+  window.open(path + img2, "qpWin", "menubar=0,resizable=0,location=no,width=480,height=500,left=800")
 }
--- _js/hlightFunctions.js
+++ _js/hlightFunctions.js
@@ -1,16 +1,15 @@
 // JavaScript Document
-
-function switchColors(it1,it2,it3,it4,it5,it6,it7,it8,it9,it10,it11,it12,it13,it14) {
-	var vColor=new Array("start",it1,it2,it3,it4,it5,it6,it7,it8,it9,it10,it11,it12,it13,it14);
-	  for(var i=1; i<17; i++) {
-		  if (vColor[i] != 0 ) {
-			  document.getElementById("chgColor"+i).style.color="#FC6907";
-		  }
-	  }
+function switchColors(it1, it2, it3, it4, it5, it6, it7, it8, it9, it10, it11, it12, it13, it14) {
+  var vColor = new Array("start", it1, it2, it3, it4, it5, it6, it7, it8, it9, it10, it11, it12, it13, it14);
+  for (var i = 1; i < 17; i++) {
+    if (vColor[i] != 0) {
+      document.getElementById("chgColor" + i).style.color = "#FC6907";
+    }
+  }
 }
-		
-function restoreColors()  {
-	for (var i=1; i<17; i++) {
-		document.getElementById("chgColor"+i).style.color="#09F";
-	}
-} 
+
+function restoreColors() {
+  for (var i = 1; i < 17; i++) {
+    document.getElementById("chgColor" + i).style.color = "#09F";
+  }
+}
