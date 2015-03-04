8cf05343fc84b81db25cdc64e39a386e65bfecc4
Andrew Pong <a@awp.io>
2015-03-04 00:20:44 -0800

    Installed BetterCMS via nuget

--- dst-website-cms/dst-website-cms/Global.asax.cs
+++ dst-website-cms/dst-website-cms/Global.asax.cs
@@ -5,17 +5,85 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Security.Principal;
+using BetterCms.Core;
+using BetterCms.Core.Environment.Host;
 
 namespace dst_website_cms
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        //protected void Application_Start()
+        //{
+        //    AreaRegistration.RegisterAllAreas();
+        //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
+        //    BundleConfig.RegisterBundles(BundleTable.Bundles);
+        //}
+
+        private static ICmsHost cmsHost;
+
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            cmsHost = CmsContext.RegisterHost();
+
+            /* DO NOT FORGET TO REMOVE DEFAULT ROUTE REGISTRATION! 
+               FOLLOWING SOURCE CODE SHOULD BE REMOVED: 
+
+               routes.MapRoute(
+                        name: "Default",
+                        url: "{controller}/{action}/{id}",
+                        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                    );
+            */
+
+            // [YOUR CODE]  
+
+            cmsHost.OnApplicationStart(this);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            // [YOUR CODE]
+
+            cmsHost.OnBeginRequest(this);
+        }
+
+        protected void Application_EndRequest()
+        {
+            // [YOUR CODE]
+
+            cmsHost.OnEndRequest(this);
+        }
+
+        protected void Application_Error()
+        {
+            // [YOUR CODE]
+
+            cmsHost.OnApplicationError(this);
+        }
+
+        protected void Application_End()
+        {
+            // [YOUR CODE]
+
+            cmsHost.OnApplicationEnd(this);
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            // [YOUR CODE]
+
+            // Uncomment following source code for a quick Better CMS test if you don't have implemented users authentication. 
+            // Do not use this code for production!
+            /*
+            var roles = new[] { "BcmsEditContent", "BcmsPublishContent", "BcmsDeleteContent", "BcmsAdministration" };
+            var principal = new GenericPrincipal(new GenericIdentity("TestUser"), roles);
+            HttpContext.Current.User = principal;
+            */
+
+            cmsHost.OnAuthenticateRequest(this);
         }
     }
+    
 }
